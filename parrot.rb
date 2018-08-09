9771e35c3e5ae42f2bae01ce7bef03c2226b9748
Devon Darrow <ddarrowphl@gmail.com>
2018-08-09 18:54:50 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,5 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(call = "Squawk!")
+puts "#{call}"
+call
+end  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
